cc58a97017291f346ab2f7f70975130277cac56f
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-07-23 18:30:15 -0300

    implementado break no laço de repetição/implemented break in loop

--- 8-loops.js
+++ 8-loops.js
@@ -18,12 +18,14 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada;
 
 let contador = 0;
+let destinoExiste = false;
 
 while(contador < 4) {
     if(listaDeDestinos[contador] == destino) {
-        console.log('Destino existe!');
-    } else {
-        console.log('Destino não existe!');
+        destinoExiste = true;
+        break;
     }
     contador += 1;
 }
+
+console.log("Destino existe: ", destinoExiste);
