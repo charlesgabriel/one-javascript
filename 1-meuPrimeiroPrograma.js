dd3120fcaedff7167dde2e335723e99dda9207cf
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-07-23 08:08:28 -0300

    introdução a linguagem/introduction to language

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,9 @@
+console.log("Meu primeiro programa: trabalhando com variáveis");
+
+const idade = 33;
+
+console.log(idade);
+console.log(idade + 2);
+
+const idadeSomada = idade + 3;
+console.log(idadeSomada);
