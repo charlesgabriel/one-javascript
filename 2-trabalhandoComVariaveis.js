f12714f7559329057339c39543508ad1a312895b
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-07-23 08:23:31 -0300

    trabalhado com variáveis/worked with variables

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,15 @@
+// Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+// JS é case sensitive
+
+idade = 33;
+console.log(idade);
+
+// Idade = 30;
+// console.log(Idade);
+
+const idade = 33;
+const nome = "Charles";
+
+// Não fazer
+// mes = "Janeiro";
