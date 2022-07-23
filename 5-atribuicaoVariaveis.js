6208e0b5611c36709e53b2fe9eda0ce66e90e4ba
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-07-23 09:57:26 -0300

    trabalhado com atribuição de variáveis/worked with attribution of variables

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,16 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 33;
+const primeiroNome = "Charles";
+const sobrenome = "Carmo";
+
+// console.log(primeiroNome + " " + sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + " " + sobrenome;
+
+console.log(nomeCompleto);
+
+let contador = 0;
+contador = contador + 1;
